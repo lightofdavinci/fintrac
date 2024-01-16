89ed63f8a6b5ed9e027b40984956650e96694555
Dan Volosnikov <danvolosnikov@gmail.com>
2024-01-15 19:52:52 -0500

    init

---
+++ src/main/java/com/fintrac/springbootwithtsreact/SpringBootWithTsReactApplication.java
@@ -0,0 +1,13 @@
+package com.fintrac.springbootwithtsreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithTsReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithTsReactApplication.class, args);
+	}
+
+}
