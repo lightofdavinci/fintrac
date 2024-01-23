959bdf96fb193dd86f4689f6029a438d951fcbc4
Dan Volosnikov <danvolosnikov@gmail.com>
2024-01-23 16:00:14 -0500

    enrollment page

--- src/main/fintrac-frontend/src/pages/Enrollment.tsx
+++ src/main/fintrac-frontend/src/pages/Enrollment.tsx
@@ -1,25 +1,40 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { ArrowLeft } from 'react-bootstrap-icons';
+import enrollmentBg from '../assets/images/enrollment.jpg';
+import './Enrollment.css'
 
 function Enrollment() {
-  const [count, setCount] = useState(0)
-
+//   const [count, setCount] = useState(0)
   return (
     <>
-      <h1>Enrollment page</h1>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <img className="enrollment-bg" src={enrollmentBg} alt="crowd"/>
+      <div className="enrollment-container">
+        <Link to={`/`} className="enrollment-btn-back"><ArrowLeft color="#333" size={24} /></Link>
+        <h1>Thanks for enrolling</h1>
+        <h2>We need a bit more info to verify your identity.</h2>
+        <form className="enrollment-form">
+          <div className="form-group">
+            <input type="text" required/><label>Username</label>
+          </div>
+          <div className="form-row">
+            <div className="form-group">
+              <input type="text" required/><label>First name</label>
+            </div>
+            <div className="form-group">
+              <input type="text" required/><label>Last name</label>
+            </div>
+          </div>
+          <div className="form-group">
+            <input type="email" required/><label>Email</label>
+          </div>
+          <div className="form-group">
+            <input type="password" required/><label>Password</label>
+          </div>
+          <button className="flex-centered" type="submit">Register</button>
+          <Link to={`/`}>Already Registered User? Click here to login</Link>
+        </form>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Link to={`/`}>Home</Link>
     </>
   )
 }
